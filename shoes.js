644ffbdc3cc8818c84584cfb90167949dcce3d28
Pranav Mohagaokar <113004550+PranavMohagaokar@users.noreply.github.com>
2024-07-07 23:43:18 +0530

    Add files via upload

---
+++ shoes.js
@@ -0,0 +1,14 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 1500,
+    delay: 200
+});
+
+    
+    ScrollReveal().reveal('.services h1,.services_cards, .services_box ', { origin: 'left' });
+    ScrollReveal().reveal('.name, .like, .comment', { origin: 'top' });
+    ScrollReveal().reveal('.main_image', { origin: 'right' });
+    ScrollReveal().reveal('section, img', { origin: 'left' });
+    ScrollReveal().reveal('.about-img, .home-content h1, ', { origin: 'left' });
+    
